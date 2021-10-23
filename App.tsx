6d48bb6866b0d06c60821530054e7ab2245ee084
LucasVeloz <lurucell@gmail.com>
2021-10-23 09:50:19 -0300

    feat: preview

--- App.tsx
+++ App.tsx
@@ -1,16 +1,27 @@
 import React from 'react';
-import { NavigationContainer, useNavigation } from '@react-navigation/native';
 import { Dimensions, Image, StyleSheet, TouchableOpacity, View } from 'react-native';
-import { createSharedElementStackNavigator, SharedElement } from 'react-navigation-shared-element'
-import Animated, { Extrapolate, interpolate, runOnJS, useAnimatedGestureHandler, useAnimatedStyle, useSharedValue, withSpring, withTiming } from 'react-native-reanimated';
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
 import { CardStyleInterpolators } from '@react-navigation/stack';
+import { createSharedElementStackNavigator, SharedElement } from 'react-navigation-shared-element'
 import { PanGestureHandler, PinchGestureHandler, PinchGestureHandlerGestureEvent } from 'react-native-gesture-handler';
+import Animated, { Extrapolate, interpolate, runOnJS, useAnimatedGestureHandler, useAnimatedStyle, useSharedValue, withSpring, withTiming } from 'react-native-reanimated';
+
+type ScreenProps = {
+  main: undefined;
+  second: undefined;
+}
+declare global {
+  namespace ReactNavigation {
+    interface RootParamList extends ScreenProps {}
+  }
+}
+
 
 const { Navigator, Screen } = createSharedElementStackNavigator();
 
 const ImageUrl = 'https://images.stockfreeimages.com/176/sfixl/1761191.jpg'
 
-const { height } = Dimensions.get('window');
+const { height, width } = Dimensions.get('window');
 
 const Main = () => {
   const navigation = useNavigation();
@@ -33,6 +44,7 @@ const Second = () => {
   const translateX = useSharedValue(0)
   const translateY = useSharedValue(0)
   const border = useSharedValue(0);
+  const focal = useSharedValue({ x: 0, y: 0 });
 
   const translatePanX = useSharedValue(0)
   const translatePanY = useSharedValue(0)
@@ -46,7 +58,7 @@ const Second = () => {
       border.value = interpolate(event.translationY, [0, height], [1, 200], Extrapolate.CLAMP)
     },
     onEnd: (event) => {
-      if (event.translationY > (height / 2)) {
+      if (event.translationY > (height * 0.1)) {
         runOnJS(navigation.goBack)();
       } else {
         translatePanX.value = withTiming(0)
@@ -59,22 +71,32 @@ const Second = () => {
 
   const onGestureEvent = useAnimatedGestureHandler<PinchGestureHandlerGestureEvent>({
     onActive(event) {
-      console.log(event.focalY)
       scale.value = interpolate(event.scale, [1, 100], [1, 100], Extrapolate.CLAMP);
       translateX.value = event.focalX;
       translateY.value = event.focalY;
+      focal.value = {x: event.focalX, y: focal.value.y};
+      focal.value = {x: focal.value.x, y: event.focalY};
     },
     onEnd() {
       scale.value = withTiming(1);
     }
   })
 
-  const rStyle = useAnimatedStyle(() => ({ 
+  const rStyle = useAnimatedStyle(() => {
+    return { 
     transform: [
+      { translateX: focal.value.x },
+      { translateY: focal.value.y },
+      { translateX: -width/2 },
+      { translateY: -height/2 },
       { scale: scale.value },
+      { translateX: -focal.value.x },
+      { translateY: -focal.value.y },
+      { translateX: width/2 },
+      { translateY: height/2 },
     ],
     borderRadius: border.value
-  }))
+  }})
 
   const panStyle = useAnimatedStyle(() => ({ 
     transform: [
